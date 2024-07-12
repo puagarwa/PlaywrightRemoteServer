2dc7793284a7fef196410b8aa0dacf70e9b8757a
Puneet Agarwal <puagarwa@microsoft.com>
2024-07-12 11:21:00 +0530

    Add remote server

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,9 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    connectOptions: {
+      wsEndpoint: `ws://localhost:8090`,
+    },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
